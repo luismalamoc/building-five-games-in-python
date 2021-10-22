8f26cfb6a4ad227599cf828a5da618d115efa597
Luis Mauricio Alamo <luismalamoc@gmail.com>
2021-10-22 20:17:13 -0300

    feat: snake game was finished

--- snake/snake.py
+++ snake/snake.py
@@ -1,11 +1,16 @@
 # Single Snake in Python 3 for Beginners 
 # Credits to @tokyoedtech https://twitter.com/tokyoedtech
-# Follow the videos on https://www.youtube.com/watch?v=LH8WgrUWG_I&list=PLlEgNdBJEO-kXk2PyBxhSmo84hsO3HAz2&ab_channel=TokyoEdtech
+# Follow the videos on https://www.youtube.com/watch?v=BP7KMlbvtOo&list=PLlEgNdBJEO-n8k9SR49AshB9j7b5Iw7hZ&ab_channel=TokyoEdtech
 
 import turtle
 import time
+import random
 
 delay = 0.1
+
+# Score
+score = 0
+high_score = 0
 
 # set up the screen
 wn = turtle.Screen()
@@ -23,18 +28,42 @@ head.penup()
 head.goto(0,0)
 head.direction = "stop"
 
+# Snake food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0,100)
+
+segments = []
+
+# pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score: 0 High Core: 0", align="center", font=("Courier", 24, "normal"))
+
 # Functions
 def go_up():
-    head.direction = "up"
+    if head.direction != "down":
+        head.direction = "up"
 
 def go_down():
-    head.direction = "down"
+    if head.direction != "up":
+        head.direction = "down"
 
 def go_left():
-    head.direction = "left"
+    if head.direction != "right":
+        head.direction = "left"
 
 def go_right():
-    head.direction = "right"
+    if head.direction != "left":
+        head.direction = "right"
 
 def move():
     if head.direction == "up":
@@ -63,7 +92,92 @@ wn.onkeypress(go_right, "d")
 # Main game loop
 while True:
     wn.update()
+    # check for a collision with the food
+    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction = "stop"
+
+        # hide the segments
+        for segment in segments:
+            segment.goto(1000,1000)
+
+        # clear the segment list
+        segments.clear()
+
+        # reset the score
+        score = 0
+
+        # reset the delay
+        delay -= 0.1
+
+        pen.clear()
+        pen.write("Score: {} High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+
+
+    if head.distance(food) < 20:
+        # Move the food to a random spot
+        x = random.randint(-290,290)
+        y = random.randint(-290,290)
+        food.goto(x, y)
+
+        # add a segement
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        # shorten the delay
+        delay -= 0.001
+
+        # increase the score
+        score += 10
+
+        if score > high_score:
+            high_score = score
+        
+        pen.clear()
+        pen.write("Score: {} High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+
+    # Move the end segments first  in reverse order
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+
+    # Move segment 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+
     move()
+
+    # check for head collision with the body segments
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = "stop"
+
+            # hide the segments
+            for segment in segments:
+                segment.goto(1000,1000)
+
+            # clear the segment list
+            segments.clear()
+
+            # reset the score
+            score = 0
+
+            # reset the delay
+            delay -= 0.1
+
+            pen.clear()
+            pen.write("Score: {} High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+
     time.sleep(delay)
 
 wn.mainloop()
